a81c24e48328bac80b70b535880348af91e66360
Camila Brito <camilagbrito48@gmail.com>
2023-06-07 13:26:08 +0100

    Props Product - findByPrice hard-code

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,12 +1,17 @@
+import { ProductDTO } from '../models/product';
 import './style.css';
 
-export default function CardProduct(){
+type Props = {
+    product: ProductDTO;
+}
+
+export default function CardProduct({product}: Props){
     return(
         <main>
             <section className='card-container'>
                 <div className='card-item'>
-                    <h2>PC Gamer Pro</h2>
-                    <h3>R$ 1200,00</h3>
+                    <h2>{product.name}</h2>
+                    <h3>{product.price.toFixed(2)}</h3>
                 </div>
             </section>
         </main>
--- src/components/Listing/index.tsx
+++ src/components/Listing/index.tsx
@@ -1,14 +1,21 @@
 import CardProduct from '../CardProduct';
+import * as productService from '../../services/product-service'
 import './style.css';
 
+
 export default function Listing(){
+
+
     return(
         <main>
             <section className='card-products-config'>
                 <div className='card-products container'>
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
+                    {
+                        productService.findByPrice(0, 5000).map(
+                            product => <CardProduct key={product.id} product={product} />
+                            )
+                    }
+
                 </div>
             </section>
         </main>
